7e829ce1cbfdd17c3e87c997ae20c8c1a616f9e4
Klement Tan <klementtan@gmail.com>
2020-05-10 02:40:30 +0800

    Complete scrapper with basic functions

--- iata-scraper.py
+++ iata-scraper.py
@@ -2,43 +2,67 @@ from bs4 import BeautifulSoup,  NavigableString, Comment
 import requests
 import pandas as pd 
 
-base_url = 'https://www.iatatravelcentre.com/international-travel-document-news/1580226297.htm'
-df = pd.DataFrame(columns= ['country', 'info'])
+url = 'https://www.iatatravelcentre.com/international-travel-document-news/1580226297.htm'
 
-
-def parse(full_url):
-  page_content = BeautifulSoup(full_url.content, 'lxml')
-  containers = page_content.findAll('b', {'style': 'user-select: auto;'})
-  df = pd.DataFrame(columns= ['country'])
-  print(containers)
-  for item in containers:
-    try:
-      country = item.find('b', {'style': 'user-select: auto'}).text.replace('\n', '')
-      print(country)
-      print(item)
-    except:
-      country = None
-    df = df.append({'country': country}, ignore_index=True)
-  df.to_csv('iata_countries.csv')
-  return df
-
-get_url = requests.get(base_url, timeout=5)
-page_content = BeautifulSoup(get_url.content, 'lxml')
-container = page_content.find('div', {'class': 'middle'})
-
-
-
-for country_data in container.findAll('br'):
+def get_main_text(iata_url):
+  main_text = None
   try:
-    print(country_data)
-    print(country_data.next_sibling.strip())
-  except:
-    print("Cannot get")
+    response = requests.get(url)
+    if response.status_code == 200:
+      html = response.text
+      soup = BeautifulSoup(html, 'lxml')
+      main_text = soup.find('div', {'class': 'middle'})
+  except Exception as ex:
+    print(str(ex))
+  finally:
+    return main_text
+
+def get_country_info(country_container):
+  country = None
+  published_date = None
+  info = ""
+  country_info_df = pd.DataFrame(columns= ['country', 'published_date', 'info'])
+  country = country_container.text
+
+  current_container = country_container
+
+  try:
+    current_container = current_container.next_sibling
+    published_date = current_container
+  except Exception as ex:
+    print("Cannot find updated date for " + country + " error " + str(ex))
+
+  current_container = current_container.next_sibling
 
 
-# #data are all the countries
-# #print(data)
 
-# print(container.b.body)
+  try:
+    while current_container.name != 'b' :
+      if ( isinstance(current_container, str)):
+        info += current_container
+        if (len(current_container) > 0):
+          info += '\n'
+      current_container = current_container.next_sibling
+  except Exception as ex:
+    print("Cannot find travel restrictions info for " + country + " error " + str(ex))
+  country_info_df = country_info_df.append({'country': country, 'published_date': published_date, 'info': info}, ignore_index=True)
+  return country_info_df  
 
-# print(container.b.next_sibling.strip())
+
+
+
+def parse_main_text(main_text):
+  all_country_info_df = pd.DataFrame(columns= ['country', 'published_date', 'info'])
+  contry_b_tags = main_text.findAll('b')
+  for country_b_tag in contry_b_tags:
+    if country_b_tag.text == 'NOTE':
+      continue
+    else:
+      all_country_info_df = all_country_info_df.append(get_country_info(country_b_tag), ignore_index=True)
+  return all_country_info_df
+
+
+main_text = get_main_text(url)
+df = parse_main_text(main_text)
+df.to_json('IATA_data.json')
+
